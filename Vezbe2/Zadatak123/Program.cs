3746bd96e0707e959b6d79cf091871ef2fb0ee00
Dragan <milosevicdragan002@gmail.com>
2024-03-13 07:40:37 +0100

    zadatak1,2

--- Vezbe2/Zadatak123/Program.cs
+++ Vezbe2/Zadatak123/Program.cs
@@ -10,6 +10,40 @@ namespace Zadatak123
     {
         static void Main(string[] args)
         {
+            #region Zadatak1
+
+            int n = 154357;
+            List<int> list = new List<int>(n);
+            int sum = 0;
+            for (int i = 0; i < n; i++) { 
+                if(i%4 == 0)
+                {
+                    list.Add(i);
+                }
+                else
+                {
+                    list.Add(-i);
+                }
+
+            }
+            foreach (int i in list)
+            {
+                if(i%17 == 0) {
+                    sum += i;
+                }
+            }
+            Console.WriteLine("The sum of all numbers in the list whose indices are divisible by 17: " + sum);
+
+            #endregion
+
+            #region Zadatak2
+
+            HashSet<int> set = new HashSet<int>(list);
+
+            #endregion
+
+            #region Zadatak3
+            Console.ReadLine();
         }
     }
 }
