5e0133468ba442e5bb16e9adfb6d5f822398064a
Dragan <milosevicdragan002@gmail.com>
2024-03-13 10:15:35 +0100

    Create Add and Delete methods

---
+++ Vezbe1/Server/LibraryService.cs
@@ -0,0 +1,51 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class LibraryService : ILibrary
+    {
+        public bool AddNewBook(Book book)
+        {
+            bool canBeAdded = !Database.CollectionOfBooks.ContainsKey(book.Id);
+            if (canBeAdded)
+            {
+                Database.CollectionOfBooks.Add(book.Id, book);
+            }
+            return canBeAdded;
+        }
+
+        public void DeleteBook(int id)
+        {
+            if (!Database.CollectionOfBooks.Remove(id))
+            {
+                throw new FaultException<CustomException>(new CustomException("Book doesn't exists"));
+            }
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Book> GetAllBooksByAuthor(string firstName, string lastName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Book> GetBooksByGener(Genre genre)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Book> GetBooksByYear(int year)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
