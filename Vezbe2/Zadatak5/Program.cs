df4f818d8a7f264d9bf8bb32260f2d16b3010796
Dragan <milosevicdragan002@gmail.com>
2024-03-13 07:56:39 +0100

    zadatak5

--- Vezbe2/Zadatak5/Program.cs
+++ Vezbe2/Zadatak5/Program.cs
@@ -10,6 +10,22 @@ namespace Zadatak5
     {
         static void Main(string[] args)
         {
+            int count = 10;
+            Dictionary<int, List<string>> dictionary = new Dictionary<int, List<string>>(count);
+
+            for (int i = 0; i < 10; i++)
+            {
+                dictionary.Add(i, new List<string> { "Element 1" + i, "Element 2" + i, "Element 3" + i });
+            }
+            
+            foreach(int key in dictionary.Keys)
+            {
+                for(int i = 0; i < dictionary[key].Count(); i++)
+                {
+                    Console.WriteLine($"<{key}> - <{i}> - <{dictionary[key][i]}>");
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
