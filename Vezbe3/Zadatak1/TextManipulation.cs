f1d14be417065aad9a4ea49a433be904209c2c40
Dragan <milosevicdragan002@gmail.com>
2024-03-19 17:49:57 +0100

    Create dispose

---
+++ Vezbe3/Zadatak1/TextManipulation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak1
+{
+    class TextManipulation:IDisposable
+    {
+        private TextWriter textWriter;
+        private TextReader textReader;
+        private bool disposed = false;
+        string path = "";
+
+        public TextManipulation(string path)
+        {
+            this.path = path;
+        }
+
+        public string Path { get => path;}
+
+        ~TextManipulation()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if(!disposed)
+            {
+                if(disposing)
+                {
+                    if (textWriter != null)
+                    {
+                        textWriter.Dispose();
+                    }
+                    if(textReader != null) { 
+                    
+                        textReader.Dispose();
+                    }
+                }
+                disposed = true;
+            }
+        }
+    }
+}
