4d98dd74f1668f27bfaa9e649b228001de6015f8
Dragan <milosevicdragan002@gmail.com>
2024-04-13 17:10:44 +0200

    Update Program.cs

--- Vezbe5/Service/Program.cs
+++ Vezbe5/Service/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,15 @@ namespace Service
     {
         static void Main(string[] args)
         {
+            using (ServiceHost host = new ServiceHost(typeof(FileHandlingService)))
+            {
+                host.Open();
+                Console.WriteLine("Service is open, press any key to close it.");
+                Console.ReadKey();
+                host.Close();
+            }
+            Console.WriteLine("Service is closed");
+            Console.ReadKey();
         }
     }
 }
