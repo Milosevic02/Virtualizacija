eb6749c4157fc24949b456c5fc6e12ba58943c0f
Dragan <milosevicdragan002@gmail.com>
2024-04-13 17:33:46 +0200

    SendFileOptions func

--- Vezbe5/Client/Program.cs
+++ Vezbe5/Client/Program.cs
@@ -33,6 +33,27 @@ namespace Client
                     Console.WriteLine($"[ERROR] Receive Files return message:{results.ResultMessage}");
                     break;
             }
+
+            static void SendFileOption(IFileHandling proxy)
+            {
+                Console.WriteLine("Please input file that you want to sent");
+                string fileName = Console.ReadLine();
+                FileManipulationResults results =
+                    proxy.SendFile(new FileManipulationOptions(FileManipulation.GetMemoryStream(fileName), fileName));
+
+                switch (results.ResultType)
+                {
+                    case ResultTypes.Success:
+                        Console.WriteLine("File is sucessfuly sent.");
+                        break;
+                    case ResultTypes.Warning:
+                        Console.WriteLine($"[WARNING] Send File return message:{results.ResultMessage}");
+                        break;
+                    case ResultTypes.Failed:
+                        Console.WriteLine($"[ERROR] Send File return message:{results.ResultMessage}");
+                        break;
+                }
+            }
         }
     }
 }
