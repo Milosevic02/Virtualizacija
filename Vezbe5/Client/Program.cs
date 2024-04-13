c1d5fbec767b3b3e4a989cfd7f2b8e8af1be3a32
Dragan <milosevicdragan002@gmail.com>
2024-04-13 17:33:10 +0200

    ReceiveFilesOption func

--- Vezbe5/Client/Program.cs
+++ Vezbe5/Client/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,28 @@ namespace Client
     {
         static void Main(string[] args)
         {
+        }
+
+
+        static void ReceiveFilesOption(IFileHandling proxy)
+        {
+            Console.WriteLine("Please input key word for files");
+            string keyWord = Console.ReadLine();
+            FileManipulationResults results =
+            proxy.GetFiles(new FileManipulationOptions(keyWord));
+
+            switch (results.ResultType)
+            {
+                case ResultTypes.Success:
+                    FileManipulation.DownloadFiles(results);
+                    break;
+                case ResultTypes.Warning:
+                    Console.WriteLine($"[WARNING] Receive Files return message:{results.ResultMessage}");
+                    break;
+                case ResultTypes.Failed:
+                    Console.WriteLine($"[ERROR] Receive Files return message:{results.ResultMessage}");
+                    break;
+            }
         }
     }
 }
