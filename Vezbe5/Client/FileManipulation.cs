d8baf6eea932fec597950af72f32e492da921cf7
Dragan <milosevicdragan002@gmail.com>
2024-04-13 17:28:39 +0200

    DownloadFiles func

---
+++ Vezbe5/Client/FileManipulation.cs
@@ -0,0 +1,35 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    static class FileManipulation
+    {
+        public static void DownloadFiles(FileManipulationResults results)
+        {
+            var downloadPath = ConfigurationManager.AppSettings["downloadPath"];
+            if(!Directory.Exists(downloadPath))
+            {
+                Directory.CreateDirectory(downloadPath);
+            }
+            foreach(KeyValuePair<string,MemoryStream> stream in results.MemoryStremCollection)
+            {
+                string fileName = stream.Key;
+                using (FileStream fileStream = new FileStream($"{downloadPath}\\{fileName}", FileMode.Create, FileAccess.Write))
+                {
+                    stream.Value.WriteTo(fileStream);
+                    fileStream.Dispose();
+                    stream.Value.Dispose();
+                    Console.WriteLine($"Downloaded file {stream.Key}");
+
+                }
+            }
+        }
+    }
+}
