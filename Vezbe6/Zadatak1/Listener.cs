8d687f0da8e07fd0d10c9ec47dcd1bbe9e90e322
Dragan <milosevicdragan002@gmail.com>
2024-05-14 19:47:45 +0200

    Create Listener.cs

---
+++ Vezbe6/Zadatak1/Listener.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak1
+{
+    public class Listener
+    {
+        public void OnRaisedNotification(object sender, EventArgs e)
+        {
+            Console.WriteLine($"Heard it! Time {DateTime.Now}");
+        }
+    }
+}
