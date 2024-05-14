04f228997af7cd44361dbfb9c75088131b9c1129
Dragan <milosevicdragan002@gmail.com>
2024-05-14 20:10:10 +0200

    Create TransactionGenerator.cs

---
+++ Vezbe6/Zadatak2/Publisher/TransactionGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak2.Publisher
+{
+    public class TransactionGenerator
+    {
+        public delegate void TransactionEventHandler(object sender,PaymentEventArgs e);
+
+        public event TransactionEventHandler PaymentReceivedEvent;
+        public event TransactionEventHandler PaymentMadeEvent;
+
+        public void PerformTransaction(int amount)
+        {
+            if (amount > 0)
+            {
+                ReceivePayment(amount);
+            }
+            else if (amount < 0)
+            {
+                MakePayment(amount);
+            }
+        }
+
+
+        private void ReceivePayment(int amount)
+        {
+            if(PaymentReceivedEvent != null)
+            {
+                PaymentReceivedEvent(this,new PaymentEventArgs(amount,"Increase"));
+            }
+            else
+            {
+                Console.WriteLine("No subscribers!");
+            }
+        }
+
+        private void MakePayment(int amount)
+        {
+            if (PaymentMadeEvent != null)
+            {
+                PaymentMadeEvent(this, new PaymentEventArgs(amount, "Decrease"));
+            }
+            else
+            {
+                Console.WriteLine("No subscribers!");
+            }
+        }
+    }
+}
