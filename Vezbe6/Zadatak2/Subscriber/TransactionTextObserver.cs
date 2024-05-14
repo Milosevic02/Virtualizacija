ea40c027746fda4d3390bb31c38cf696102fda1d
Dragan <milosevicdragan002@gmail.com>
2024-05-14 20:10:07 +0200

    Create TransactionTextObserver.cs

---
+++ Vezbe6/Zadatak2/Subscriber/TransactionTextObserver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zadatak2.Publisher;
+
+namespace Zadatak2.Subscriber
+{
+    public class TransactionTextObserver
+    {
+        public void HandleTransaction(object sender, PaymentEventArgs e)
+        {
+            Console.WriteLine($"You got one new message >> Message: {e.Message}, Ammount: {e.Amount}");
+        }
+    }
+}
