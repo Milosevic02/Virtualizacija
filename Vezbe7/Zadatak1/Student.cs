cd31dacc07ce0a8133ec9429da3b910fdf3280c0
Dragan <milosevicdragan002@gmail.com>
2024-05-18 09:42:20 +0200

    Create Student.cs

---
+++ Vezbe7/Zadatak1/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak1
+{
+    public class Student
+    {
+        public void AlarmZvoni(Alarm alarm,EventArgs e)
+        {
+            Console.WriteLine("Da li zelite da odložite alarm? (da/ne)");
+            string odgovor = Console.ReadLine();
+
+            switch (odgovor.ToLower())
+            {
+                case "ne":
+                    alarm.Odbaci(this);
+                    break;
+                case "da":
+                    alarm.Odlozi();
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    }
+}
