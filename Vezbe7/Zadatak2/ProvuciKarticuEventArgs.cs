156fb5f562889f0109faee8d2ab00b49ee688a6a
Dragan <milosevicdragan002@gmail.com>
2024-05-18 09:55:39 +0200

    Create ProvuciKarticuEventArgs.cs

---
+++ Vezbe7/Zadatak2/ProvuciKarticuEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak2
+{
+    public class ProvuciKarticuEventArgs:EventArgs
+    {
+        private decimal stanjeNaKartici;
+        private decimal ukupnoZaduzenje;
+
+        public decimal StanjeNaKartici { get { return stanjeNaKartici; } set { stanjeNaKartici = value; } }
+        public decimal UkupnoZaduzenje { get { return ukupnoZaduzenje; } set { ukupnoZaduzenje = value; } }
+
+        public ProvuciKarticuEventArgs(decimal ukupnoZaduzenje, decimal stanjeNaKartici)
+        {
+            this.StanjeNaKartici = stanjeNaKartici;
+            this.UkupnoZaduzenje = ukupnoZaduzenje;
+        }
+    }
+}
