942037927544d03481ba3c67f2e9f7f404c73de9
Dragan <milosevicdragan002@gmail.com>
2024-05-18 09:55:47 +0200

    Create Kupac.cs

---
+++ Vezbe7/Zadatak2/Kupac.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak2
+{
+    public class Kupac
+    {
+        private decimal kartica = 500.0m;
+        public decimal Kartica { get { return kartica; } set { kartica = value; } }
+
+        public void UporediCenu(object sender,SkenirajProizvodEventArgs args)
+        {
+            Console.WriteLine($"Skeniran proizvod: {args.NazivProizvoda};");
+            Console.WriteLine($" Cena: {args.CenaProizvoda};");
+            Console.WriteLine($"--- Ukupno zaduženje: {args.IznosRacuna}---");
+        }
+    }
+}
