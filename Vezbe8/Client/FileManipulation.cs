7eb3e9a592af88c224f86f06b7b5615d3dd27afc
Dragan <milosevicdragan002@gmail.com>
2024-05-27 11:13:47 +0200

    DownloadFile func

---
+++ Vezbe8/Client/FileManipulation.cs
@@ -0,0 +1,36 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class FileManipulation
+    {
+        private FileSystemWatcher inputFileWatcher;
+        private string filePath;
+
+        public FileManipulation(string path)
+        {
+            
+        }
+
+        public void DownloadFile(FileManipulationResults results,string fileName)
+        {
+            using (FileStream fileStream = new FileStream($"{filePath}\\{fileName}", FileMode.Create, FileAccess.Write))
+            {
+                results.MemoryStream.WriteTo(fileStream);
+                fileStream.Dispose();
+                results.MemoryStream.Dispose();
+                Console.WriteLine($"Downloaded file {fileName}");
+            }
+        }
+
+
+
+
+    }
+}
