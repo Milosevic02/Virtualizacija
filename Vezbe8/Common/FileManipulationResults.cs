20bc019a4f3f5dae17ebb9b17bc9ccdd04377649
Dragan <milosevicdragan002@gmail.com>
2024-05-27 10:56:38 +0200

    Create FileManipulationResults.cs

---
+++ Vezbe8/Common/FileManipulationResults.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    [DataContract]
+    public class FileManipulationResults:IDisposable
+    {
+        [DataMember]
+        public MemoryStream MemoryStream { get; set; }
+
+        [DataMember]
+        public string ResultMessage { get; set; }
+
+        [DataMember]
+        public ResultTypes ResultType { get; set; }
+
+        public FileManipulationResults() {
+            ResultType = ResultTypes.Success;
+            MemoryStream = new MemoryStream();
+        }
+
+        public void Dispose()
+        {
+            if(MemoryStream == null)
+            {
+                return;
+            }
+            try
+            {
+                MemoryStream.Dispose();
+                MemoryStream.Close();
+                MemoryStream = null;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unsuccessful disposing");
+            }
+        }
+    }
+}
