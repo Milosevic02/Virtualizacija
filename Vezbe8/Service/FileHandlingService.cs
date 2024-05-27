f403bbfbd7e1d2daa8ffe9d2f28e74d82c35ae46
Dragan <milosevicdragan002@gmail.com>
2024-05-27 11:04:11 +0200

    Create FileHandlingService.cs

---
+++ Vezbe8/Service/FileHandlingService.cs
@@ -0,0 +1,28 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class FileHandlingService : IFileHandler
+    {
+        public FileManipulationResults GetFile(FileManipulationOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddMemoryStream(string filePath,string keyWord,FileManipulationResults results)
+        {
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                MemoryStream ms = new MemoryStream();
+                fileStream.CopyTo(ms);
+                results.MemoryStream = ms;
+            }
+        }
+    }
+}
