f883708680bc0c821642631a4951560348ea651e
Dragan <milosevicdragan002@gmail.com>
2024-05-28 18:29:43 +0200

    Create LibrarySubscriber.cs

---
+++ PripremaK2/Common/LibrarySubscriber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class LibrarySubscriber
+    {
+        public void OnRatingChanged(object sender, BookEventArgs e)
+        {
+            Console.WriteLine($"Book {e.Title} score changed from {e.OldScore} to {e.NewScore} ");
+        }
+    }
+}
