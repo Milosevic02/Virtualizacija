6606e246b6c39042347ab5bd0290041ea823eed0
Dragan <milosevicdragan002@gmail.com>
2024-05-28 18:21:00 +0200

    Create FileManipulationOptions.cs

---
+++ PripremaK2/Common/FileManipulationOptions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    [DataContract]
+    public class FileManipulationOptions
+    {
+        public FileManipulationOptions(MemoryStream memoryStream,string fileName) { 
+        
+            this.MemoryStream = memoryStream;
+            this.FileName = fileName;
+        }
+
+        [DataMember]
+        public MemoryStream MemoryStream {  get; set; }
+
+        [DataMember] 
+        public string FileName { get; set; }
+
+        public void Dispose()
+        {
+            if(MemoryStream == null) return;
+
+            try
+            {
+                MemoryStream.Dispose();
+                MemoryStream.Close();
+                MemoryStream = null;
+            }catch(Exception) {
+
+                Console.WriteLine("Unsuccessful disposing");
+            }
+
+        }
+
+    }
+}
