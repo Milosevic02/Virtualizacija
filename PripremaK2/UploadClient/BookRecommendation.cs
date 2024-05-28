7780047f45d65cd1cd197c70652ed26bd0127e61
Dragan <milosevicdragan002@gmail.com>
2024-05-28 19:08:55 +0200

    GetMemoryStream func

--- PripremaK2/UploadClient/BookRecommendation.cs
+++ PripremaK2/UploadClient/BookRecommendation.cs
@@ -31,5 +31,45 @@ namespace UploadClient
             fileWatcher.Changed += FileChanged;
             fileWatcher.EnableRaisingEvents = true;
         }
+
+        private void FileChanged(object sender, FileSystemEventArgs e)
+        {
+            
+        }
+
+
+        public static MemoryStream GetMemoryStream(string path)
+        {
+            if(!File.Exists(path))
+            {
+                Console.WriteLine($"Cannot process the file because directory not exists.");
+                return null;
+            }
+
+            MemoryStream memoryStream = new MemoryStream();
+            FileStream fileStream = null;
+            try
+            {
+                fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+                fileStream.CopyTo(memoryStream);
+                fileStream.Close();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Cannot process the file {path}. Message: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Something went wrong: {path}.  Message: {e.Message}");
+            }
+            finally
+            {
+                if( fileStream != null)
+                {
+                    fileStream.Dispose();
+                }
+            }
+            return memoryStream;
+        }
     }
 }
