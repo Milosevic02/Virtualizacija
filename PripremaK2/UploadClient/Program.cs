485a60db402ee5032add36ba7f4750c1ab8e5400
Dragan <milosevicdragan002@gmail.com>
2024-05-28 19:15:51 +0200

    PrintMenu func

--- PripremaK2/UploadClient/Program.cs
+++ PripremaK2/UploadClient/Program.cs
@@ -11,5 +11,20 @@ namespace UploadClient
         static void Main(string[] args)
         {
         }
+
+        static int PrintMenu()
+        {
+            Console.WriteLine("Select an option");
+            Console.WriteLine("1. Add book recommendation");
+            Console.WriteLine("2. Exit and provide content for recommendations");
+            if (Int32.TryParse(Console.ReadLine(), out int number))
+            {
+                if (number >= 1 && number <= 2)
+                {
+                    return number;
+                }
+            }
+            return 0;
+        }
     }
 }
