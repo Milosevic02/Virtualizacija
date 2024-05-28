9618e4af53b831df381e5a3eaf1226dce0908e31
Dragan <milosevicdragan002@gmail.com>
2024-05-28 19:04:38 +0200

    CreateFileSystemWatcher func

---
+++ PripremaK2/UploadClient/BookRecommendation.cs
@@ -0,0 +1,35 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UploadClient
+{
+    public class BookRecommendation
+    {
+        private FileSystemWatcher fileWatcher;
+        private readonly ILibrary proxy;
+
+        public BookRecommendation(string path,ILibrary sender)
+        {
+            CreateFileSystemWatcher(path);
+            this.proxy = sender;
+        }
+
+        private void CreateFileSystemWatcher(string path)
+        {
+            fileWatcher = new FileSystemWatcher()
+            {
+                Path = path,
+                Filter = "*.*",
+                NotifyFilter = NotifyFilters.LastWrite
+            };
+
+            fileWatcher.Changed += FileChanged;
+            fileWatcher.EnableRaisingEvents = true;
+        }
+    }
+}
