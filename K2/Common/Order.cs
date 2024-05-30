cb9940567915f5ead4d7324481ebc1d51ffa9093
Dragan <milosevicdragan002@gmail.com>
2024-05-30 12:31:24 +0200

    Create Order.cs

---
+++ K2/Common/Order.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    [DataContract]
+    public class Order
+    {
+        private int id;
+        private string path;
+        private bool delivered;
+        public Order(int id, string path)
+        {
+            this.id = id;
+            this.path = path;
+            this.delivered = false;
+        }
+
+        public Order() { }
+
+        [DataMember]
+        public int Id { get { return id;} set { id = value; } }
+        [DataMember]
+        public string Path { get { return path; } set { path = value; }  }
+        [DataMember]
+        public bool Delivered { get { return delivered; } set { delivered = value; } }
+
+        
+
+    }
+}
