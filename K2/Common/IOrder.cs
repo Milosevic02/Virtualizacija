6298325ad134048cd7d3c11e2d51aed53febaaed
Dragan <milosevicdragan002@gmail.com>
2024-05-30 12:45:41 +0200

    Create IOrder.cs

---
+++ K2/Common/IOrder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    [ServiceContract]
+    public interface IOrder
+    {
+        [OperationContract]
+        void SendOrderRequest(OrderDetails orderDetails);
+
+        [OperationContract]
+        List<Order>GetAllOrders();
+
+        [OperationContract]
+        [FaultContract(typeof(CustomException))]
+        void ProcessDelivery(string orderId, int fee);
+
+        [OperationContract]
+        void Subscribe();
+
+        [OperationContract]
+        void Unsubscribe();
+    }
+}
