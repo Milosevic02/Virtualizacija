2353a0cf5a4297d759d1b82617f0d49aacd82f64
Dragan <milosevicdragan002@gmail.com>
2024-05-30 12:40:17 +0200

    Create OrderDetails.cs

---
+++ K2/Common/OrderDetails.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    [DataContract]
+    public class OrderDetails:IDisposable
+    {
+        [DataMember]
+        public MemoryStream MemoryStream {  get; set; }
+
+        [DataMember]
+        public string FileName { get; set; }
+
+        public void Dispose()
+        {
+            if(MemoryStream == null)
+            {
+                return;
+            }
+            try
+            {
+                MemoryStream.Dispose();
+                MemoryStream.Close();
+                MemoryStream = null;
+            }catch(Exception)
+            {
+                Console.WriteLine("Unsuccessful disposing");
+            }
+        }
+    }
+}
