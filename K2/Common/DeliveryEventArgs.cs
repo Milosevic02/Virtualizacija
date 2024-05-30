87922a4394f8295bb1f5549804d4f183f4ae3a85
Dragan <milosevicdragan002@gmail.com>
2024-05-30 12:34:27 +0200

    Create DeliveryEventArgs.cs

---
+++ K2/Common/DeliveryEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class DeliveryEventArgs:EventArgs
+    {
+        private int id;
+        private int tips;
+
+        public DeliveryEventArgs(int id, int tips)
+        {
+            this.id = id;
+            this.tips = tips;
+        }
+
+        public int Id { get { return id; } set { id = value; } }
+        public int Tips { get { return tips; } set { tips = value; } }
+    }
+}
