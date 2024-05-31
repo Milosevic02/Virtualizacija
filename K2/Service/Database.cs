d355b7b384a841616f4d4a2fdaadddcfe7afe5a2
Dragan <milosevicdragan002@gmail.com>
2024-05-31 09:27:43 +0200

    Create Database.cs

---
+++ K2/Service/Database.cs
@@ -0,0 +1,22 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class Database
+    {
+        readonly static Dictionary<string, Order> collectionOfOrders;
+
+        static Database()
+        {
+            collectionOfOrders = new Dictionary<string, Order>();
+
+        }
+
+        public static Dictionary<string, Order> CollectionOfOrders { get { return collectionOfOrders; } }
+    }
+}
