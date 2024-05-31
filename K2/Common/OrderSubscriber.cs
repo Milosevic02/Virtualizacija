33f3f5ef8207f5cdbe628b3bb1b2da3e93ca850e
Dragan <milosevicdragan002@gmail.com>
2024-05-31 09:27:54 +0200

    Create OrderSubscriber.cs

---
+++ K2/Common/OrderSubscriber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class OrderSubscriber
+    {
+        public void OnFeeChanged(object sender, DeliveryEventArgs e)
+        {
+            Console.WriteLine($"Order with ID:{e.Id}  Tips is {e.Tips}");
+        }
+    }
+}
